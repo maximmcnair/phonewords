f39c22dd9112c00a34e287bda14e0ed49d7ff820
Maxim McNair <mail@maximmcnair.com>
2018-01-11 21:38:01 +0000

    add clear btn and app styling

---
+++ src/components/NumberClear.js
@@ -0,0 +1,7 @@
+import React, {PureComponent} from 'react'
+
+export default function NumberClear({handleClear}) {
+  return (
+    <a className="numberclear__btn" onClick={handleClear}>Clear</a>
+  )
+}
