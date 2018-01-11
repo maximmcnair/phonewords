38045717d02525d55b3797fa96f8ef93d0c8a156
Maxim McNair <mail@maximmcnair.com>
2018-01-11 18:53:46 +0000

    added basic inmemory cache

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const app = express()
 
 const generatePhonewords = require('./lib/generate-phonewords')
 
+// NOTE
+// this would be moved into a db in a real project, every time app is restarted
+// cache will be destory. This also won't work across different processes.
+const inMemoryCache = {}
+
 app.get('/phonewords', (req, res) => {
+  if (!req.query.numbers) return res.status(400).send('Error: No numbers sent')
+  if (inMemoryCache[req.query.numbers]) return res.json(inMemoryCache[req.query.numbers])
   const phonewords = generatePhonewords(req.query.numbers)
+  inMemoryCache[req.query.numbers] = phonewords
   res.json(phonewords)
 })
 
